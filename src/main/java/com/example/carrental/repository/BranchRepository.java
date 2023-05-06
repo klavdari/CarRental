4bdc2367cd10e93a51ec92d7cd0d99f6f01fe9d2
Krisel <kristiclavdari@gmail.com>
2023-05-07 00:48:52 +0200

    added queries

--- src/main/java/com/example/carrental/repository/BranchRepository.java
+++ src/main/java/com/example/carrental/repository/BranchRepository.java
@@ -3,8 +3,10 @@ package com.example.carrental.repository;
 import com.example.carrental.model.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface BranchRepository extends JpaRepository<Branch,Integer> {
 
-
+    List<Branch> findByRentalId(int rentalId);
 }
--- src/main/java/com/example/carrental/repository/CarRepository.java
+++ src/main/java/com/example/carrental/repository/CarRepository.java
@@ -2,6 +2,12 @@ package com.example.carrental.repository;
 
 import com.example.carrental.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car,Integer> {
+
+    @Query(value = "select c from Car c where c.branchLocated.id = ?1")
+    List<Car> findCarsByBranchId(int branchId);
 }
