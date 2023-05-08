a2e600698addda15b110418e518e87968818bf91
Krisel <kristiclavdari@gmail.com>
2023-05-09 00:28:55 +0200

    minor query changes

--- src/main/java/com/example/carrental/repository/BranchRepository.java
+++ src/main/java/com/example/carrental/repository/BranchRepository.java
@@ -2,11 +2,13 @@ package com.example.carrental.repository;
 
 import com.example.carrental.model.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface BranchRepository extends JpaRepository<Branch,Integer> {
 
+    @Query(value = "select b from Branch b where b.rental.id = ?1")
     List<Branch> findByRentalId(int rentalId);
 }
