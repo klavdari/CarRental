904f9a26d77f7ef22ef11b3fd4232458c141696a
Krisel <kristiclavdari@gmail.com>
2023-05-20 11:21:50 +0200

    custom queries

--- src/main/java/com/example/carrental/repository/CarRepository.java
+++ src/main/java/com/example/carrental/repository/CarRepository.java
@@ -3,11 +3,11 @@ package com.example.carrental.repository;
 import com.example.carrental.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface CarRepository extends JpaRepository<Car,Integer> {
 
-    @Query(value = "select c from Car c where c.branchLocated.id = ?1")
-    List<Car> findCarsByBranchId(int branchId);
 }
--- src/main/java/com/example/carrental/repository/ReservationRepository.java
+++ src/main/java/com/example/carrental/repository/ReservationRepository.java
@@ -2,6 +2,19 @@ package com.example.carrental.repository;
 
 import com.example.carrental.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
+
+    @Query( "SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END "
+            + "FROM Reservation r "
+            + "WHERE r.car.id = :carId "
+            + "AND ((r.dateFrom <= :startDate AND r.dateTo >= :endDate) "
+            + "OR (r.dateFrom <= :startDate AND r.dateTo >= :endDate) "
+            + "OR (r.dateFrom >= :startDate AND r.dateTo <= :endDate))")
+    boolean isCarBooked(int carId,LocalDate startDate,LocalDate endDate);
+
+
 }
